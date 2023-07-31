fe9296dba10983b170d5066cc7e967a31c31f935
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-07-31 20:48:47 +0300

    done with basic html structuring and display clicked number in display box

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 let firstNumber;
 let secondNumber;
 let operator;
+let displayBox = document.querySelector(".display-box");
 
 // functions for add, sub, mult and div
 function add(firstNumber, secondNumber) {
@@ -32,4 +33,13 @@ function operate(operator, number1, number2) {
   }
 }
 
-console.log(operate("+", 10, 5));
+//Displaying clicked NUmber in the display box
+let allNumbers = document.querySelectorAll(".numbers");
+allNumbers = Array.from(allNumbers);
+allNumbers.forEach((number) => {
+  number.addEventListener("click", function () {
+    displayBox.textContent = number.textContent;
+  });
+});
+//console.log(allNumbers);
+//console.log(operate("+", 10, 5));
