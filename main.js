566a4dff6fc33f956bc976f77ba3e8a9cff7dfbf
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-08-01 11:58:53 +0300

    refactoring codes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// two input numbers and operator
+// JavaScript
 let firstNumber = [];
 let secondNumber = [];
 let operatorsButtons = document.querySelectorAll(".operators");
@@ -7,8 +7,7 @@ let placedOperator;
 let equalOperator = document.querySelector(".equalOperator");
 let isOperatorClicked = false;
 let clearBtn = document.querySelector(".clear-btn");
-console.log(clearBtn);
-// functions for add, sub, mult and div
+
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 }
@@ -22,7 +21,6 @@ function div(firstNumber, secondNumber) {
   return firstNumber / secondNumber;
 }
 
-//operate function
 function operate(operator, number1, number2) {
   if (operator === "+") {
     return add(number1, number2);
@@ -37,48 +35,37 @@ function operate(operator, number1, number2) {
   }
 }
 
-//Displaying first number in the display box
+// Event listener for numbers
 let allNumbers = document.querySelectorAll(".numbers");
-allNumbers = Array.from(allNumbers);
 allNumbers.forEach((number) => {
   number.addEventListener("click", function () {
+    if (!isOperatorClicked) {
+      firstNumber.push(Number(number.textContent));
+    } else {
+      secondNumber.push(Number(number.textContent));
+    }
     displayBox.textContent += number.textContent;
-    firstNumber.push(number.textContent);
   });
 });
 
-// iterate through operators
+// Event listener for operators
 operatorsButtons.forEach((operator) => {
   operator.addEventListener("click", function () {
     placedOperator = operator.textContent;
     displayBox.textContent = operator.textContent;
-    localStorage.setItem("firstNumber", JSON.stringify(firstNumber));
     isOperatorClicked = true;
-
-    // Add the event listener for the second number input here
-    allNumbers.forEach((number) => {
-      number.addEventListener("click", function () {
-        secondNumber.push(number.textContent);
-      });
-    });
-    // displaying second number in display box
-    for (let i = 0; i < secondNumber.length; i++) {
-      displayBox.textContent += secondNumber[i];
-    }
   });
 });
 
-// equalOperator
+// Event listener for equalOperator
 equalOperator.addEventListener("click", function () {
-  firstNumber = Number(
-    JSON.parse(localStorage.getItem("firstNumber")).join("")
-  );
-  secondNumber = Number(secondNumber.join(""));
+  firstNumber = firstNumber.reduce((acc, num) => acc * 10 + num, 0);
+  secondNumber = secondNumber.reduce((acc, num) => acc * 10 + num, 0);
   let result = operate(placedOperator, firstNumber, secondNumber);
   displayBox.textContent = result;
 });
 
-// clear functionality
+// Event listener for clear button
 clearBtn.addEventListener("click", function () {
   firstNumber = [];
   secondNumber = [];
