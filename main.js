0f944799fcfbbd98dba14ef7f3eaaa62864b81c9
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-08-01 11:15:57 +0300

    done with applying add,sub,mult and div functionality to GUI

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // two input numbers and operator
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = [];
+let secondNumber = [];
+let operatorsButtons = document.querySelectorAll(".operators");
 let displayBox = document.querySelector(".display-box");
+let placedOperator;
+let equalOperator = document.querySelector(".equalOperator");
+let isOperatorClicked = false;
 
 // functions for add, sub, mult and div
 function add(firstNumber, secondNumber) {
@@ -33,13 +36,43 @@ function operate(operator, number1, number2) {
   }
 }
 
-//Displaying clicked NUmber in the display box
+//Displaying first number in the display box
 let allNumbers = document.querySelectorAll(".numbers");
 allNumbers = Array.from(allNumbers);
 allNumbers.forEach((number) => {
   number.addEventListener("click", function () {
-    displayBox.textContent = number.textContent;
+    displayBox.textContent += number.textContent;
+    firstNumber.push(number.textContent);
   });
 });
-//console.log(allNumbers);
-//console.log(operate("+", 10, 5));
+
+// iterate through operators
+operatorsButtons.forEach((operator) => {
+  operator.addEventListener("click", function () {
+    placedOperator = operator.textContent;
+    displayBox.textContent = operator.textContent;
+    localStorage.setItem("firstNumber", JSON.stringify(firstNumber));
+    isOperatorClicked = true;
+
+    // Add the event listener for the second number input here
+    allNumbers.forEach((number) => {
+      number.addEventListener("click", function () {
+        secondNumber.push(number.textContent);
+      });
+    });
+    // displaying second number in display box
+    for (let i = 0; i < secondNumber.length; i++) {
+      displayBox.textContent += secondNumber[i];
+    }
+  });
+});
+
+// equalOperator
+equalOperator.addEventListener("click", function () {
+  firstNumber = Number(
+    JSON.parse(localStorage.getItem("firstNumber")).join("")
+  );
+  secondNumber = Number(secondNumber.join(""));
+  let result = operate(placedOperator, firstNumber, secondNumber);
+  displayBox.textContent = result;
+});
